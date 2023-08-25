5a53dddd4305b41342472214c684a4e75b6603ce
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-08-25 08:46:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+for (let index = 0; index < 5; index++) {
+	window.promises.push(new Promise((resolve) => {
+		setTimeout(() => {
+			document.querySelector("div").textContent = "Something";
+			resolve(true)
+		}, Math.floor(Math.random() * 5))
+	}))
+}
