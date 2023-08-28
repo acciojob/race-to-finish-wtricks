bf96bc01ecbc4df51ddca4576e0352c4f4675c2f
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-08-28 22:36:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,21 +7,30 @@ function getRandomTime() {
   return Math.floor(Math.random() * 5000) + 1000; // Between 1000ms and 5000ms
 }
 
-const promises = Array.from({ length: 5 }, () =>
-	{
-		let t =  getRandomTime();
-		new Promise((resolve) => setTimeout(resolve, t,t))
-	}
-);
+for(let i =0; i<5;i++) {
+	window.promises.push(Math.floor(Math.random() * 5))
+}
 
-window.promises.push(...promises)
+// const promises = Array.from({ length: 5 }, () =>
+// 	{
+// 		let t =  getRandomTime();
+// 		new Promise((resolve) => setTimeout(resolve, t,t))
+// 	}
+// );
 
-Promise.any(promises)
-  .then((result) => {
-    // Print the result to the output div.
-    const outputDiv = document.getElementById("output");
-    outputDiv.textContent = `First resolved result: ${result}`;
-  })
-  .catch((error) => {
-    console.error("All promises failed:", error);
-  });
+// window.promises.push(...promises)
+
+// Promise.any(promises)
+//   .then((result) => {
+//     // Print the result to the output div.
+//     const outputDiv = document.getElementById("output");
+//     outputDiv.textContent = `First resolved result: ${result}`;
+//   })
+//   .catch((error) => {
+//     console.error("All promises failed:", error);
+//   });
+
+
+
+
+// const promises = window.promises.map(delay => { return new Promise(resolve => { setTimeout(() => { resolve(delay); }, delay * 1000); }); }); Promise.any(promises).then(result => { res = result; }); })
