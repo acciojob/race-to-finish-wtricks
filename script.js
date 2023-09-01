7be54efa5f6e30dda6abb92377ee92e20eb0d1ab
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-09-01 10:07:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,35 +2,22 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+const outputDiv = document.getElementById('output');
 
-function getRandomTime() {
-  return Math.floor(Math.random() * 5000) + 1000; // Between 1000ms and 5000ms
-}
+const getRandomTime = () => Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds
 
-for(let i =0; i<5;i++) {
-	window.promises.push(Math.floor(Math.random() * 5))
-}
+window.promises = [
+    new Promise(resolve => setTimeout(() => resolve("Promise 1 resolved"), getRandomTime())),
+    new Promise(resolve => setTimeout(() => resolve("Promise 2 resolved"), getRandomTime())),
+    new Promise(resolve => setTimeout(() => resolve("Promise 3 resolved"), getRandomTime())),
+    new Promise(resolve => setTimeout(() => resolve("Promise 4 resolved"), getRandomTime())),
+    new Promise(resolve => setTimeout(() => resolve("Promise 5 resolved"), getRandomTime()))
+];
 
-// const promises = Array.from({ length: 5 }, () =>
-// 	{
-// 		let t =  getRandomTime();
-// 		new Promise((resolve) => setTimeout(resolve, t,t))
-// 	}
-// );
-
-// window.promises.push(...promises)
-
-// Promise.any(promises)
-//   .then((result) => {
-//     // Print the result to the output div.
-//     const outputDiv = document.getElementById("output");
-//     outputDiv.textContent = `First resolved result: ${result}`;
-//   })
-//   .catch((error) => {
-//     console.error("All promises failed:", error);
-//   });
-
-
-
-
-// const promises = window.promises.map(delay => { return new Promise(resolve => { setTimeout(() => { resolve(delay); }, delay * 1000); }); }); Promise.any(promises).then(result => { res = result; }); })
+Promise.any(promises)
+    .then(result => {
+        outputDiv.textContent = result;
+    })
+    .catch(error => {
+        console.error('All promises rejected:', error);
+    });
